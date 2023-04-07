40b76f4b8d13d4565da1a34453df64dee749302b
SilviGrigoryan09 <silvana.g@tumo.org>
2023-04-07 20:20:10 +0400

    first commit

---
+++ gameOfLife/flower.js
@@ -0,0 +1,122 @@
+class Flower {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.energy = 16
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+
+
+    chooseCell(char) {
+        this.getNewCoordinates()
+        let found = []
+
+
+        for (let i in this.directions) {
+            let x = this.directions[i][0]
+            let y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i])
+                }
+            }
+        }
+
+
+        return found
+
+    }
+
+    mul() {
+        let emptyCell = this.chooseCell(0)
+        let newCell = random(emptyCell)
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+             for(let i in this.directions){
+
+                let x = this.directions[i][0]
+                let y = this.directions[i][1]
+                if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                    matrix[y][x] = 11
+                    let flow = new Flower(newX, newY)
+                    matrix[this.y][this.x] = 4
+                    flowerArr.push(flow)
+                }
+
+            }
+            
+         
+
+
+            
+        }
+    }
+
+
+
+    eat(){
+        let emptyCell = this.chooseCell(3)
+        let newCell = random(emptyCell)
+
+           if(newCell ){
+               this.energy+=12
+            let newX = newCell[0]
+            let newY = newCell[1]
+                   for(let i in predatorArr){
+                            if(newX == predatorArr[i].x  && newY == predatorArr[i].y){
+                                      predatorArr.splice(i,1)
+                            }
+                   }
+
+                   matrix[newY][newX] = 4
+                 
+                   
+
+                   matrix[this.y][this.x] = 0
+
+
+                   this.x = newX
+                   this.y = newY
+                   if(this.energy > 25){
+                        this.mul()
+                   }
+
+           }else {
+             this.energy--
+           }
+
+           if(this.energy < 0){
+            this.die ()
+           }
+     }
+
+
+     die(){
+        matrix[this.y][this.x] = 0
+
+          for(let i in flowerArr){
+                   if(this.x == flowerArr[i].x && this.y == flowerArr[i].y) {
+                             flowerArr.splice(i,1)
+                   }
+    }
+    
+   
+}
+
+}
---
+++ gameOfLife/gardener.js
@@ -0,0 +1,132 @@
+class Gardener {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.energy = 20
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+
+
+    chooseCell(char2) {
+        this.getNewCoordinates()
+        let found = []
+
+
+        for (let i in this.directions) {
+            let x = this.directions[i][0]
+            let y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char2) {
+                   found.push(this.directions[i])
+                }
+            }
+        }
+
+
+        return found
+
+    }
+
+
+
+
+    mul() {
+        let emptyCell = this.chooseCell(2)
+        let newCell = random(emptyCell)
+
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            matrix[newY][newX] = 5
+
+            let gard = new Gardener(newX, newY)
+
+            gardenerArr.push(gard)
+
+
+        }
+    }
+
+
+    eat() {
+        let emptyCell = this.chooseCell(3)
+        let newCell = random(emptyCell)
+
+        if (newCell) {
+            this.energy += 6
+            let newX = newCell[0]
+            let newY = newCell[1]
+
+            for (let i in predatorArr) {
+                if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                    predatorArr.splice(i, 1)
+                }
+            }
+
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+
+            this.x = newX
+            this.y = newY
+
+            if (this.energy > 20) {
+                this.mul()
+            }
+
+        } else {
+            this.move()
+        }
+    }
+
+    move(){
+        let emptyCell = this.chooseCell(0)
+        let newCell = random(emptyCell)
+
+            if(newCell){
+                let newX = newCell[0]
+                let newY = newCell[1]
+
+                matrix[newY][newX] = 5
+                matrix[this.y][this.x] = 0
+                
+                this.x = newX
+                this.y = newY
+
+                this.energy--
+
+                if(this.energy < 0){
+                    this.die ()
+                }
+            }
+     }
+
+     die(){
+        matrix[this.y][this.x] = 0
+
+          for(let i in gardenerArr){
+                   if(this.x == gardenerArr[i].x && this.y == gardenerArr[i].y) {
+                             gardenerArr.splice(i,1)
+                   }
+          }
+    }
+    
+
+
+
+}
