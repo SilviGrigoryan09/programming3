a7518000c26187931fec176cc64b2a12ee389c39
SilviGrigoryan09 <silvana.g@tumo.org>
2023-04-09 19:37:10 +0400

    Js class inheritance

--- gameOfLife/gardener.js
+++ gameOfLife/gardener.js
@@ -1,7 +1,6 @@
-class Gardener {
+class Gardener extends LivingCreature{
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        super(x,y)
         this.energy = 20
         this.directions = [];
     }
@@ -24,21 +23,7 @@ class Gardener {
     chooseCell(char2) {
         this.getNewCoordinates()
         let found = []
-
-
-        for (let i in this.directions) {
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char2) {
-                   found.push(this.directions[i])
-                }
-            }
-        }
-
-
-        return found
-
+        return super.chooseCell(char)
     }
 
 
--- gameOfLife/grass.js
+++ gameOfLife/grass.js
@@ -1,19 +1,7 @@
-class Grass{
+class Grass extends LivingCreature{
     constructor(x,y){
-        this.x = x
-        this.y = y
+        super(x,y)
         this.multiply = 0
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x    , this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y    ],
-            [this.x + 1, this.y    ],
-            [this.x - 1, this.y + 1],
-            [this.x    , this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    
     }
     
 
